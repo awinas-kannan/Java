18ca42c643d82793c969d439b120a33c5ea80363
GBS05349 <GBS05349@NBKPS408.SG.GBS.PRO>
2021-04-12 17:01:20 +0530

    Java 8 Streams

---
+++ src/main/java/com/awinas/learning/Java8/streams/Streamz.java
@@ -0,0 +1,29 @@
+package com.awinas.learning.Java8.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Streamz {
+
+	public static void main(String[] args) {
+		List<String> memberNames = new ArrayList<>();
+		memberNames.add("Amitabh");
+		memberNames.add("Shekhar");
+		memberNames.add("Aman");
+		memberNames.add("Rahul");
+		memberNames.add("Shahrukh");
+		memberNames.add("Salman");
+		memberNames.add("Yana");
+		memberNames.add("Lokesh");
+
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 1; i < 10; i++) {
+			list.add(i);
+		}
+		Stream<Integer> stream = list.stream();
+		System.out.println(stream.count());
+
+	}
+
+}
